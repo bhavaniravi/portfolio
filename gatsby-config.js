7c92e397d64e3e5fb2017126fe005387eb6c17ae
Bhavani Ravi <bhava0895@gmail.com>
2019-01-06 01:42:16 +0530

    move sitemetadata to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//     siteMetadata: {
-//         "title":"Bhavani Ravi",
-//         "about_me": "I am what I am",
-//         "tagline": "The geekette"
-//       },
-//     plugins: [
-//       `gatsby-plugin-emotion`,
-//       {
-//         resolve: `gatsby-plugin-typography`,
-//         options: {
-//           pathToConfigModule: `src/utils/typography`,
-//         },
-//       },
-//     ],
-//   }
+module.exports = {
+    siteMetadata: {
+        "title":"Bhavani Ravi",
+        "about_me": "🔸 Building @kissflow at @orangescape \n 🔸 Code - Speak - Write \n 🔸 Python - Chatbots - ML \n 🔸  womenintech  🔸",
+        "tagline": "The Geekette"
+      },
+    plugins: [
+        {
+            resolve: `gatsby-source-medium`,
+            options: {
+              username: `@bhavaniravi`,
+              limit: 200,
+            },
+        }
+    ]
+}
