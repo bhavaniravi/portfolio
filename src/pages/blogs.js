99b4ed919e1cdbc804923dc2b02f84efa3d218f2
Bhavani Ravi <bhava0895@gmail.com>
2019-01-06 01:41:45 +0530

    add a page to list all blogs

---
+++ src/pages/blogs.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import BlogPageList from "../components/blog_page"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default () => (
+    <Layout>
+        <BlogPageList></BlogPageList>
+    </Layout>
+  )
