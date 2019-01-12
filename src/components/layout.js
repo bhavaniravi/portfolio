27596cec449d523ed33d0a79129ee0bbb858d6fc
Bhavani Ravi <bhava0895@gmail.com>
2019-01-12 20:25:40 +0530

    fix css issues and bootstrap loading issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,13 @@
 import React from "react"
-import Helemt from "react-helmet";
+import Helmet from "react-helmet";
 import FooterArea from "./footer";
 import Header from "./header"
 import { withPrefix } from 'gatsby'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
 
 export default ({ navFixed, children }) => (
     <div>
-    <Helemt>
+    <Helmet>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <link rel="icon" href="static/img/favicon.png" type="image/png"/>
@@ -22,13 +22,12 @@ export default ({ navFixed, children }) => (
         <link rel="stylesheet" href={withPrefix("vendors/nice-select/css/nice-select.css")}/>
         <link rel="stylesheet" href={withPrefix("vendors/animate-css/animate.css")}/>
         <link rel="stylesheet" href={withPrefix("vendors/popup/magnific-popup.css")}/>
-        
         <link rel="stylesheet" href={withPrefix("css/style.css")}/>
         <link rel="stylesheet" href={withPrefix("css/responsive.css")}/>
         <link rel="stylesheet" href={withPrefix("css/medium_blog.css")}/>
        
         
-    </Helemt>
+    </Helmet>
     <Header navFixed={navFixed}></Header>
       {children}
       <FooterArea></FooterArea>  
