9c4b8b6767bad8c25f19b1a6de251c0e6caa85ce
Bhavani Ravi <bhava0895@gmail.com>
2019-01-15 19:48:29 +0530

    edit resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,13 +5,12 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout navFixed={true}>
-        {/* <div className="container"> */}
-        <iframe title="resume" className="iframe_styles" style={{marginTop: "100px"}}
-        src="https://docs.google.com/document/d/e/2PACX-1vQQNiou2C6dvZR0UJhBTu5ETVICI1dhq9i0sdAtdjhR6c_QssrqhkFEkFBgvK-_oVEQ-NZJPVjvBfa9/pub?embedded=true"
-        width="1200px"
-        height="1200px"
-        ></iframe>
-        
-        {/* </div> */}
+        <div className="container">
+            <iframe title="resume" className="iframe_styles" style={{marginTop: "100px"}}
+            src="https://docs.google.com/document/d/e/2PACX-1vQQNiou2C6dvZR0UJhBTu5ETVICI1dhq9i0sdAtdjhR6c_QssrqhkFEkFBgvK-_oVEQ-NZJPVjvBfa9/pub?embedded=true"
+            width="100%"
+            height="1000px"
+            ></iframe>`
+        </div>
     </Layout>
 )
