62d3f712cc1d3876bc983ff3a1a010fc51e89cef
Bhavani Ravi <bhava0895@gmail.com>
2019-06-07 17:47:40 +0530

    google site verification tag added

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta name="google-site-verification" content="7AW50PusvVdoc_XrPFyKtEmfSNOIDRpXPqyANxnLl_s" />
           <title>Bhavani's Portfolio</title>
           <meta name="twitter:site" content="@geeky_bhavani" />
           <meta name="twitter:creator" content="@geeky_bhavani" />
