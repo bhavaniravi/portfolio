c67d58dbe92d4877d55892740aabcca816227d37
Bhavani Ravi <bhava0895@gmail.com>
2019-06-07 18:09:44 +0530

    fix rss feed issue'

--- gatsby-config.js
+++ gatsby-config.js
@@ -201,11 +201,63 @@ module.exports = {
                 plugins: [],
               },
         },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+              query: `
+                {
+                  site {
+                    siteMetadata {
+                      title
+                      description
+                      siteUrl
+                      site_url: siteUrl
+                    }
+                  }
+                }
+              `,
+              feeds: [
+                {
+                  serialize: ({ query: { site, allMarkdownRemark } }) => {
+                    return allMarkdownRemark.edges.map(edge => {
+                      return Object.assign({}, edge.node.frontmatter, {
+                        description: edge.node.excerpt,
+                        date: edge.node.frontmatter.date,
+                        url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                        guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                        custom_elements: [{ "content:encoded": edge.node.html }],
+                      })
+                    })
+                  },
+                  query: `
+                    {
+                      allMarkdownRemark(
+                        sort: { order: DESC, fields: [frontmatter___published_date] },
+                      ) {
+                        edges {
+                          node {
+                            excerpt
+                            html
+                            fields { slug }
+                            frontmatter {
+                              title
+                              published_date
+                            }
+                          }
+                        }
+                      }
+                    }
+                  `,
+                  output: "/rss.xml",
+                  title: "Bhavani Ravi's Blog Feed",
+                },
+              ],
+            },
+        },
         `gatsby-remark-copy-linked-files`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         "gatsby-plugin-sitemap",
-        `gatsby-plugin-feed`,
         'gatsby-plugin-robots-txt',
         {
             resolve: `gatsby-remark-images`,
