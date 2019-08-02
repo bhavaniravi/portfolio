24b37566c5401a70a58096527f30998197e270db
Bhavani Ravi <bhava0895@gmail.com>
2019-08-02 16:05:21 +0530

    add heatmap

--- src/html.js
+++ src/html.js
@@ -20,10 +20,12 @@ export default class HTML extends React.Component {
           {this.props.headComponents}
 
           <script>
-(function(h,e,a,t,m,p) {
-m=e.createElement(a);m.async=!0;m.src=t;
-p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
-})(window,document,'script','https://u.heatmap.it/log.js');
+            (function(h,e,a,t,m,p) {
+            m=e.createElement(a);
+            m.async=!0;
+            m.src=t;
+            p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
+            })(window,document,'script','https://u.heatmap.it/log.js');
 </script>
 
 
