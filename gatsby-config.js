781110ff438ec48ccdfe563d54bf0a7d57531943
bhavaniravi <bhava0895@gmail.com>
2020-04-28 10:24:02 +0530

    add published date to RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -350,7 +350,7 @@ module.exports = {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
+                  date: edge.node.frontmatter.published_date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ "content:encoded": edge.node.html }],
