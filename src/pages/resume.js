ca31876f077b61bd689759e28d880fc261247120
bhavaniravi <bhava0895@gmail.com>
2021-01-10 13:46:16 +0530

    resume v10

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,7 +7,7 @@ export default () => (
     <Layout navFixed={true}>
         <div className="container">
             <iframe title="resume" className="iframe_styles" style={{marginTop: "100px"}}
-            src="https://docs.google.com/document/d/e/2PACX-1vQQNiou2C6dvZR0UJhBTu5ETVICI1dhq9i0sdAtdjhR6c_QssrqhkFEkFBgvK-_oVEQ-NZJPVjvBfa9/pub?embedded=true"
+            src="https://drive.google.com/file/d/1HTPsIlmXeglyBfvaTUYZ1u4aongFnPZe/view?usp=sharing"
             width="100%"
             height="1000px"
             ></iframe>`
