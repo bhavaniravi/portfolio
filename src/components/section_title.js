b2c0ea49c3c6d725e9f7ee29e0d06840954da35a
bhavaniravi <bhava0895@gmail.com>
2022-03-06 23:11:53 +0530

    fix multiple headings

--- src/components/section_title.js
+++ src/components/section_title.js
@@ -5,8 +5,8 @@ export default class SectionTitle extends Component{
         return(
             <div className="container">
         		<div className="main_title">
-        			<h1>{this.props.title}</h1>
-        			<h2>{this.props.sub_title}</h2>
+        			<h2>{this.props.title}</h1>
+        			<h3>{this.props.sub_title}</h2>
         		</div>
         	</div>
         )
